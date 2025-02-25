a45f42389542d8d20b9b0928878bd048ad6942d4
Titusz87 <titus.tarcsai@gmail.com>
2025-02-25 11:16:43 +1030

    Update chat.js

--- Chatting_App/chat.js
+++ Chatting_App/chat.js
@@ -1,16 +1,17 @@
 /*
 
 • Write a ‘chat’ command-line application:
+
 o Start-up arguments:
- A username;
- The middleware endpoint (TCP port).
+  o A username;
+  o The middleware endpoint (TCP port).
 
 o Behaviour:
- Upon starting up, it should subscribe to the above topic;
- Any messages that other users post should be printed to the console, along
-with the username of whoever posted the message;
- Any messages entered by the user (terminated by a carriage return) should
-be sent to the chat room for the other participants to see.
+  o Upon starting up, it should subscribe to the above topic;
+  o Any messages that other users post should be printed to the console, along
+    with the username of whoever posted the message;
+  o Any messages entered by the user (terminated by a carriage return) should
+    be sent to the chat room for the other participants to see.
 
 Final Product
 • Expose the chat application over a simple GUI interface in which multiple users can log in and
