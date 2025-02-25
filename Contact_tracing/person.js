40cc9b346a086782f5ffc75f32ea52333d0702ff
Titusz87 <titus.tarcsai@gmail.com>
2025-02-25 11:18:27 +1030

    Update person.js

--- Contact_tracing/person.js
+++ Contact_tracing/person.js
@@ -16,6 +16,14 @@ o Behaviour:
       publish that move to the ‘position’ topic. (Movement speed should accord
       with the start-up argument provided).
 
+
+Final Product
+• Provide a GUI that:
+    o Gives a visual representation of the ‘environment’; and
+    o Allows a user to query a person identifier and to see with whom they have come into
+      contact.
+• Extend the solution to work for boards/environments of different sizes.
+
 */
 
 
