224dd5f3e9d4b615b0a163f9655c31596bdb15a2
Titusz87 <titus.tarcsai@gmail.com>
2025-02-25 11:18:57 +1030

    Update tracker.js

--- Contact_tracing/tracker.js
+++ Contact_tracing/tracker.js
@@ -16,6 +16,14 @@ o Behaviour:
         the ‘query-response’ topic with all the names that person came into
         contact with onto the console in reverse-chronological order.
 
+
+Final Product
+• Provide a GUI that:
+    o Gives a visual representation of the ‘environment’; and
+    o Allows a user to query a person identifier and to see with whom they have come into
+      contact.
+• Extend the solution to work for boards/environments of different sizes.
+
 */
 
 
